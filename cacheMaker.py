b4b2c9755e3f3108568a6bdc1861fecf4e47c543
peterkodermac <peter@kodermac.com>
2013-09-08 20:45:48 +0200

    Create cacheMaker.py

---
+++ cacheMaker.py
@@ -0,0 +1,27 @@
+import mechanize
+from BeautifulSoup import BeautifulSoup
+
+sitemapUrl="http://www.raspberryweather.com/sitemap.xml"
+parsedUrl=[] #contains urls from sitemap
+
+browser=mechanize.Browser()
+try:
+        response=browser.open(sitemapUrl)
+except:
+        print "Something went wrong"
+        exit()
+
+soup=BeautifulSoup(response)
+
+for url in soup.findAll('loc'):
+        url=str(url)
+        url=url[5:]                     # i know i know, could be done nicer
+        parsedUrl.append(url[:len(url)-6])
+        
+#visit the sites and generate a cache
+for url in parsedUrl:
+        print "Visiting: "+url
+        browser.open(url)
+        print "Done, cache created"
+
+print "Finished, if you want to check, have a look at /wp-content/cache"
